b0ee131a07a42585945ecdbbe88bda5003f174ee
Thibault REMY <thibremy@hotmail.com>
2019-07-21 20:25:09 -0300

    chore(doc): add nav links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,5 +4,9 @@ module.exports = {
   description: 'Vue function to discover if you are in a server or browser',
   themeConfig: {
     search: false,
-  }
+    nav: [
+      { text: 'Github', link: 'https://github.com/thibremy/vue-fn-ssr' },
+      { text: 'NPM', link: 'https://www.npmjs.com/package/vue-fn-ssr' },
+    ],
+  },
 }
